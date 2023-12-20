b621f0a4279cee7f42545b1616eae1b3cb7dc9b8
Charlène Baldi <charlene.baldi6@gmail.com>
2023-12-20 12:56:08 +0100

    ajout des event listeners et affichage des dots

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,64 @@
+// Carrousel
+
 const slides = [
 	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		"image": "slide1.jpg",
+		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		"image": "slide2.jpg",
+		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		"image": "slide3.jpg",
+		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		"image": "slide4.png",
+		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
-]
+];
+console.log(slides);
+
+// Constantes des flèches
+
+const ArrowLeft = document.querySelector(".arrow_left");
+const ArrowRight = document.querySelector(".arrow_right");
+
+// Autres variables
+
+const dots = document.querySelector(".dots");
+let index = 0;
+
+// Affichage des dots
+
+function displayDots() {
+	for (let i = 0; i < slides.length; i++) {
+		const dot = document.createElement("div");
+		dot.classList.add("dot");
+		dots.appendChild(dot);
+		if (i == index) {
+			dot.classList.add("dot_selected");
+		}
+	}
+}
+displayDots();
+
+// Clic gauche
+function leftclick() {
+	ArrowLeft.addEventListener("click", () => {
+		console.log("left");
+	});
+}
+leftclick();
+
+// Clic droit
+function rightclick() {
+	ArrowRight.addEventListener("click", () => {
+		console.log("right");
+	});
+}
+rightclick();
+
+
+
